ad3296408a8695c776f88ddb289e45744afc4cb4
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2023-12-18 00:06:59 +0800

    chore: 错别字

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,4 @@ export { default as Banner } from './Banner';
 export { default as LinkCardWall } from './LinkCardWall';
 export { default as Tabs } from './Tabs';
 
-export { default as ComponentTransition } from './ComponentTrainsition';
+export { default as ComponentTransition } from './ComponentTransition';
