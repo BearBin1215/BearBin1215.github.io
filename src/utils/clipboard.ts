9241652efb74d94b2e162e4540a9268e593f22b4
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2023-12-31 20:42:53 +0800

    feat: 备用util

---
+++ src/utils/clipboard.ts
@@ -0,0 +1,43 @@
+/**
+ * 复制文本进入剪贴板
+ */
+const copyText = async (text = ''): Promise<void> => {
+  if (navigator.clipboard?.writeText) {
+    await navigator.clipboard.writeText(text);
+  } else {
+    const input = document.createElement('input');
+    input.style.position = 'fixed';
+    input.style.top = '-10000px';
+    input.style.zIndex = '-999';
+    input.style.opacity = '0';
+    document.body.appendChild(input);
+    input.value = text;
+    input.focus();
+    input.select();
+    document.execCommand('copy');
+    document.body.removeChild(input);
+  }
+};
+
+/**
+ * 从剪贴板读取文本
+ */
+const pasteText = async (): Promise<string> => {
+  if (navigator.clipboard && 'readText' in navigator.clipboard) {
+    return await navigator.clipboard.readText();
+  }
+  const pasteArea = document.createElement('textarea');
+  document.body.appendChild(pasteArea);
+  pasteArea.style.position = 'absolute';
+  pasteArea.style.top = '0';
+  pasteArea.style.zIndex = '-999';
+  pasteArea.focus();
+  const text = pasteArea.value;
+  document.body.removeChild(pasteArea);
+  return text;
+};
+
+export {
+  copyText,
+  pasteText
+};
