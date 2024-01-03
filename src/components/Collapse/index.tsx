49b42991ef014ffe48ce23954c2ab6a07e7a0ad5
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2024-01-03 10:02:05 +0800

    feat: 折叠组件动画

--- src/components/Collapse/index.tsx
+++ src/components/Collapse/index.tsx
@@ -1,8 +1,7 @@
 /**
  * @description 折叠组件
- * @todo 折叠动画
  */
-import React, { useState, HTMLAttributes } from 'react';
+import React, { useState, useRef, useEffect, HTMLAttributes } from 'react';
 import classNames from 'classnames';
 
 interface CollapseProps extends HTMLAttributes<HTMLDivElement> {
@@ -25,6 +24,28 @@ interface CollapseProps extends HTMLAttributes<HTMLDivElement> {
 const Collapse: React.FC<CollapseProps> = ({ expanded = false, label, children, className, ...props }) => {
   const [isExpanded, setExpanded] = useState(expanded);
 
+  const contentWrapperRef = useRef<HTMLDivElement>(null);
+
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (contentWrapperRef.current && !expanded) {
+      contentWrapperRef.current.style.height = '0';
+    }
+  }, []);
+
+  const handleExpand = () => {
+    if (contentWrapperRef.current) {
+      // 根据折叠情况，设置高度为0或内容高度
+      if (isExpanded) {
+        contentWrapperRef.current.style.height = '0';
+      } else {
+        contentWrapperRef.current.style.height = `${contentRef.current?.offsetHeight}px`;
+      }
+    }
+    setExpanded(!isExpanded);
+  };
+
   return (
     <div
       className={classNames('bearui-collapse', `bearui-collapse-${isExpanded ? 'expended' : 'collapsed'}`, className)}
@@ -32,14 +53,20 @@ const Collapse: React.FC<CollapseProps> = ({ expanded = false, label, children, 
     >
       <div
         className='bearui-collapse-label'
-        onClick={() => setExpanded(!isExpanded)}
+        onClick={handleExpand}
       >
         {label || (isExpanded ? '折叠' : '展开')}
       </div>
       <div
-        className='bearui-collapse-content'
+        className='bearui-collapse-content-wrapper'
+        ref={contentWrapperRef}
       >
-        {children}
+        <div
+          className='bearui-collapse-content'
+          ref={contentRef}
+        >
+          {children}
+        </div>
       </div>
     </div>
   );
