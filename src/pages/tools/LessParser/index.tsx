99023175ec162bfd7775d343385dbdd28883278c
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2024-01-28 00:34:46 +0800

    feat: Less解析器高亮显示，复制及下载功能

--- src/pages/tools/LessParser/index.tsx
+++ src/pages/tools/LessParser/index.tsx
@@ -1,10 +1,14 @@
 import React, { useEffect, useState, useRef } from 'react';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { a11yLight } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import { render as parseLess } from 'less';
 import debounce from 'lodash/debounce';
+import { Button } from '@/components';
+import { copyText } from '@/utils/clipboard';
+import saveFile from '@/utils/saveFile';
 import './index.scss';
 
-const defaultLessCode = `
-.foo {
+const defaultLessCode = `.foo {
   color: #000;
 
   .bar {
@@ -15,10 +19,12 @@ const defaultLessCode = `
     }
   }
 }
-`.trim();
+`;
 
 const StyleParser: React.FC = () => {
   const [lessParseResult, setLessParseResult] = useState('');
+  const [isSucess, setIsSuccess] = useState(true);
+
   const inputRef = useRef<HTMLTextAreaElement>(null);
 
   useEffect(() => {
@@ -35,10 +41,35 @@ const StyleParser: React.FC = () => {
    * 输入框代码发生变化后若0.5s内无变化则进行解析
    */
   const handleLessCodeChange = debounce(({ target: { value } }: React.ChangeEvent<HTMLTextAreaElement>) => {
-    parseLess(value)
-      .then(({ css }) => setLessParseResult(css))
-      .catch((error) => setLessParseResult(error));
+    parseLess(value, (error, output) => {
+      console.log(error, output);
+      if (error) {
+        setLessParseResult(error.message);
+        setIsSuccess(false);
+      } else {
+        setLessParseResult(output!.css);
+        setIsSuccess(true);
+      }
+    });
   }, 500);
+
+  /**
+   * 复制结果
+   */
+  const handleCopy = () => {
+    if (isSucess) {
+      copyText(lessParseResult);
+    }
+  };
+
+  /**
+   * 将结果下载为css文件
+   */
+  const handleDownload = () => {
+    if (isSucess) {
+      saveFile(new Blob([lessParseResult]), 'output.css');
+    }
+  };
 
   return (
     <>
@@ -49,11 +80,30 @@ const StyleParser: React.FC = () => {
           ref={inputRef}
           className='code'
         />
-        <textarea
+        <div className='button-area'>
+          {/* <Button>
+            上传
+          </Button> */}
+          <Button
+            onClick={handleCopy}
+            buttonType='primary'
+          >
+            复制
+          </Button>
+          <Button
+            onClick={handleDownload}
+          >
+            下载
+          </Button>
+        </div>
+        <SyntaxHighlighter language='css' style={a11yLight}>
+          {lessParseResult}
+        </SyntaxHighlighter>
+        {/* <textarea
           readOnly
           value={lessParseResult}
           className='code'
-        />
+        /> */}
       </div>
       本来还想搓一个scss解析器上去，结果发现scss只能在node上运行（
     </>
---
+++ src/utils/saveFile.ts
@@ -0,0 +1,13 @@
+/**
+ * 将Blob对象保存为文件
+ */
+const saveFile = (blob: Blob, fileName: string) => {
+  const link = document.createElement('a');
+  link.href = URL.createObjectURL(blob);
+  link.download = fileName;
+
+  document.body.appendChild(link);
+  link.click();
+};
+
+export default saveFile;
