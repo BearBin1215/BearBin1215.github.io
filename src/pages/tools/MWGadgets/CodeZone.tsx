2061e52116771a546506aae12b593c4328d8f847
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2024-02-07 15:41:10 +0800

    feat: 调整样式

--- src/pages/tools/MWGadgets/CodeZone.tsx
+++ src/pages/tools/MWGadgets/CodeZone.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { RiFileCopyLine } from 'react-icons/ri';
-import SyntaxHighlighter from 'react-syntax-highlighter';
-import { a11yLight } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import js from 'react-syntax-highlighter/dist/esm/languages/prism/javascript';
+import { duotoneLight } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import { copyText } from '@/utils/clipboard';
 
 const CodeZone: React.FC<{ gadgetName: string }> = ({ gadgetName }) => {
+  useEffect(() => {
+    SyntaxHighlighter.registerLanguage('javascript', js);
+  }, []);
+
   const loaderCode = `mw.loader.load("https://cdn.jsdelivr.net/gh/BearBin1215/MoegirlPedia@master/dist/gadgets/${gadgetName}.min.js");`;
 
   const copyCode = () => {
@@ -22,7 +27,7 @@ const CodeZone: React.FC<{ gadgetName: string }> = ({ gadgetName }) => {
       </button>
       <SyntaxHighlighter
         language='javascript'
-        style={a11yLight}
+        style={duotoneLight}
         className='gadget-codehighlight'
       >
         {loaderCode}
