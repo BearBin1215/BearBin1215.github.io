f45b7153f4d8100a97906d533394b472b2446427
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2024-02-19 10:36:30 +0800

    feat:

--- src/pages/tools/MWGadgets/CodeZone.tsx
+++ src/pages/tools/MWGadgets/CodeZone.tsx
@@ -3,6 +3,7 @@ import { RiFileCopyLine } from 'react-icons/ri';
 import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
 import js from 'react-syntax-highlighter/dist/esm/languages/prism/javascript';
 import { duotoneLight } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { Tabs } from '@/components';
 import { copyText } from '@/utils/clipboard';
 
 const CodeZone: React.FC<{ gadgetName: string }> = ({ gadgetName }) => {
@@ -10,29 +11,49 @@ const CodeZone: React.FC<{ gadgetName: string }> = ({ gadgetName }) => {
     SyntaxHighlighter.registerLanguage('javascript', js);
   }, []);
 
-  const loaderCode = `mw.loader.load("https://cdn.jsdelivr.net/gh/BearBin1215/MoegirlPedia@master/dist/gadgets/${gadgetName}.min.js");`;
+  const loaderCodeCDN = `mw.loader.load("https://cdn.jsdelivr.net/gh/BearBin1215/MoegirlPedia@master/dist/gadgets/${gadgetName}.min.js");`;
 
-  const copyCode = () => {
-    copyText(loaderCode);
-  };
+  const loaderCodeUserPage = `mw.loader.load("/index.php?title=User:BearBin/js/${gadgetName}.js&action=raw&ctype=text/javascript");`;
 
   return (
-    <div className='gadget-codezone'>
-      <button
-        className='gadget-copycode'
-        onClick={copyCode}
-        title='复制'
-      >
-        <RiFileCopyLine className='copy-icon' />
-      </button>
-      <SyntaxHighlighter
-        language='javascript'
-        style={duotoneLight}
-        className='gadget-codehighlight'
-      >
-        {loaderCode}
-      </SyntaxHighlighter>
-    </div>
+    <Tabs className='mgpgadgets-tabs'>
+      <Tabs.Tab label='外部CDN'>
+        <div className='gadget-codezone'>
+          <button
+            className='gadget-copycode'
+            onClick={() => copyText(loaderCodeCDN)}
+            title='复制'
+          >
+            <RiFileCopyLine className='copy-icon' />
+          </button>
+          <SyntaxHighlighter
+            language='javascript'
+            style={duotoneLight}
+            className='gadget-codehighlight'
+          >
+            {loaderCodeCDN}
+          </SyntaxHighlighter>
+        </div>
+      </Tabs.Tab>
+      <Tabs.Tab label='站内用户JS'>
+        <div className='gadget-codezone'>
+          <button
+            className='gadget-copycode'
+            onClick={() => copyText(loaderCodeUserPage)}
+            title='复制'
+          >
+            <RiFileCopyLine className='copy-icon' />
+          </button>
+          <SyntaxHighlighter
+            language='javascript'
+            style={duotoneLight}
+            className='gadget-codehighlight'
+          >
+            {loaderCodeUserPage}
+          </SyntaxHighlighter>
+        </div>
+      </Tabs.Tab>
+    </Tabs>
   );
 };
 
