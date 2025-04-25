11e2390a1bb5aacefb5e9069333330b16eebd811
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2025-04-25 21:02:17 +0800

    feat(json-view): 代码框高亮

--- src/pages/tools/json-view/index.tsx
+++ src/pages/tools/json-view/index.tsx
@@ -1,19 +1,23 @@
 import React, { useState, useEffect, ChangeEventHandler } from 'react';
-import ReactJson, { type InteractionProps } from 'react-json-view';
-import packageJson from '../../../../package.json';
+import ReactJson from 'react18-json-view';
+import CodeMirror from '@uiw/react-codemirror';
+import { json } from '@codemirror/lang-json';
+import { EditorView } from '@codemirror/view';
 import { Banner } from '@/components';
+import 'react18-json-view/src/style.css'
 import './index.scss';
+import packageJson from '../../../../package.json';
 
 const JSONView: React.FC = () => {
   const [src, setSrc] = useState(packageJson);
   const [text, setText] = useState(JSON.stringify(packageJson, null, 2));
 
-  const handleEdit = (edit: InteractionProps) => {
-    setText(JSON.stringify(edit.updated_src, null, 2));
+  const handleEdit = ({ src }: { src: any }) => {
+    setText(JSON.stringify(src, null, 2));
   };
 
-  const handleTextChange: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
-    setText(e.target.value);
+  const handleTextChange = (value: string) => {
+    setText(value);
   };
 
   useEffect(() => {
@@ -30,7 +34,11 @@ const JSONView: React.FC = () => {
       <Banner type='header'>
         使用组件：
         <a href='https://github.com/mac-s-g/react-json-view'>
-          react-json-view
+          react1-json-view
+        </a>
+        、
+        <a href='https://github.com/uiwjs/react-codemirror'>
+          @uiw/react-codemirror
         </a>
       </Banner>
       <div id='json-view'>
@@ -39,10 +47,14 @@ const JSONView: React.FC = () => {
           onEdit={handleEdit}
           onAdd={handleEdit}
           onDelete={handleEdit}
+          editable
+          theme='vscode'
         />
-        <textarea
-          id='json-text'
+        <CodeMirror
+          className='json-editor'
           value={text}
+          maxHeight='35em'
+          extensions={[json(), EditorView.lineWrapping]}
           onChange={handleTextChange}
         />
       </div>
