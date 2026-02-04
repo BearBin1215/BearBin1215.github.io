9f0cb6cd0f64295d213ae1f095febf700d412cf2
bearbin1215 <20369414+BearBin1215@users.noreply.github.com>
2026-02-04 17:07:11 +0800

    feat: 高亮

--- src/pages/tools/oritech-calculator/index.tsx
+++ src/pages/tools/oritech-calculator/index.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.scss';
 
 const OritechCalculator = () => {
   const speedList = [...Array(10).keys()];
   const processList = [...Array(10).keys()];
+
+  const [selectedCount, setSelectedCount] = useState<number>(0);
 
   // 计算处理时间的函数
   const calculateTime = (speed: number, process: number) => {
@@ -18,19 +20,38 @@ const OritechCalculator = () => {
       <p>
         假定处理一个物品初始用时200tick（10s），本表计算处理64个物品所用时间（s）。
       </p>
+      <p>
+        点击第一列/行数字可高亮。
+      </p>
       <table className='oritech-calculator'>
         <tbody>
           <tr>
             <td>加工\速度</td>
             {speedList.map((speed) => (
-              <td key={speed}>{speed}</td>
+              <td
+                key={speed}
+                className={selectedCount === speed ? 'highlight' : ''}
+                onClick={() => setSelectedCount(speed)}
+                style={{ cursor: 'pointer' }}
+              >
+                {speed}
+              </td>
             ))}
           </tr>
           {processList.map((process) => (
             <tr key={process}>
-              <td>{process}</td>
+              <td
+                className={selectedCount === process ? 'highlight' : ''}
+                onClick={() => setSelectedCount(process)}
+                style={{ cursor: 'pointer' }}
+              >
+                {process}
+              </td>
               {speedList.map((speed) => (
-                <td key={`${process}-${speed}`}>
+                <td
+                  key={`${process}-${speed}`}
+                  className={process + speed === selectedCount ? 'highlight' : ''}
+                >
                   {calculateTime(speed, process)}
                 </td>
               ))}
