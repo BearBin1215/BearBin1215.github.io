21b250c13fd627dd213bf24605ed9cbdf6f6108d
bearbin1215 <20369414+BearBin1215@users.noreply.github.com>
2026-03-20 17:03:45 +0800

    build(deps): 简化配置

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,7 @@ import svgr from "vite-plugin-svgr";
 /** esm 中模拟 cjs 的 __dirname */
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-export default defineConfig(({ mode }) => {
-  const isProd = mode === 'production';
-
+export default defineConfig(() => {
   return {
     plugins: [
       react(),
@@ -20,7 +18,6 @@ export default defineConfig(({ mode }) => {
       }),
     ],
     resolve: {
-      extensions: ['.tsx', '.ts', '.js', '.jsx'],
       alias: {
         '@': path.resolve(__dirname, 'src'),
       },
@@ -29,11 +26,9 @@ export default defineConfig(({ mode }) => {
     server: {
       port: 8090,
       open: true,
-      // static 目录在 Vite 中默认为 public，原 ./dist 作为输出目录
     },
     build: {
       outDir: 'dist',
-      sourcemap: !isProd,
     },
   };
 });
